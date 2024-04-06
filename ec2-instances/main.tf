4df17c52ebe37d8638313332c87bdda4d8f30084
tampubolon <tampubolonmartinus@gmail.com>
2024-04-06 18:26:53 +0700

    m-tone:distribute sbnet,create pub&pvt ec2,udarta ins ES

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -18,8 +18,16 @@ variable "ami" {
 
 locals {
   vpc_id = "vpc-078d43214673a89d4"
-
   ec2_instances = yamldecode(file(var.ec2_yaml_file))
+
+  public_subnet_count  = length(tolist(data.aws_subnet_ids.public.ids))
+  private_subnet_count = length(tolist(data.aws_subnet_ids.private.ids))
+
+  public_subnet_indices = range(local.public_subnet_count)
+  private_subnet_indices = range(local.private_subnet_count)
+
+  public_instance_count = length(local.ec2_instances.public)
+  private_instance_count = length(local.ec2_instances.private)
 }
 
 data "aws_subnet_ids" "public" {
@@ -101,46 +109,47 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   role = aws_iam_role.ec2_role.name
 }
 
+
 resource "aws_instance" "public_ec2_instances" {
-  for_each = { for idx, ec2_instance in local.ec2_instances.public : idx => ec2_instance }
+  count = local.public_instance_count
 
   ami                         = var.ami
-  instance_type               = each.value.instance_type
-  subnet_id                   = tolist(data.aws_subnet_ids.public.ids)[0] # Fix subnet_id assignment
+  instance_type               = local.ec2_instances.public[count.index].instance_type
+  subnet_id                   = element(tolist(data.aws_subnet_ids.public.ids), local.public_subnet_indices[count.index % local.public_subnet_count])
   key_name                    = "elasticsearch"
   associate_public_ip_address = true
 
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
   tags = {
-    Name = each.value.name
+    Name = local.ec2_instances.public[count.index].name
   }
 
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 
   user_data = templatefile("${path.module}/user_data.tpl", {
-    elasticsearch = each.value.elasticsearch
+    elasticsearch = local.ec2_instances.public[count.index].elasticsearch
   })
 }
 
 resource "aws_instance" "private_ec2_instances" {
-  for_each = { for idx, ec2_instance in local.ec2_instances.private : idx => ec2_instance }
+  count = local.private_instance_count
 
   ami                         = var.ami
-  instance_type               = each.value.instance_type
-  subnet_id                   = tolist(data.aws_subnet_ids.private.ids)[0] # Fix subnet_id assignment
+  instance_type               = local.ec2_instances.private[count.index].instance_type
+  subnet_id                   = element(tolist(data.aws_subnet_ids.private.ids), local.private_subnet_indices[count.index % local.private_subnet_count])
   key_name                    = "elasticsearch"
   associate_public_ip_address = false
 
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
   tags = {
-    Name = each.value.name
+    Name = local.ec2_instances.private[count.index].name
   }
 
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 
   user_data = templatefile("${path.module}/user_data.tpl", {
-    elasticsearch = each.value.elasticsearch
+    elasticsearch = local.ec2_instances.private[count.index].elasticsearch
   })
 }
